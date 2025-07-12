32d6b12767b52acd3f104b3737f7cf2d98778f4d
Elias-cdv <elways38@gmail.com>
2025-07-12 09:14:33 -0500

    submit all changes

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,45 @@
+public class Journal
+{
+    private List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry entry)
+    {
+        _entries.Add(entry);
+    }
+
+    public void DisplayAll()
+    {
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+        }
+    }
+
+    public void SaveToFile(string filename)
+    {
+        using (StreamWriter outputFile = new StreamWriter(filename))
+        {
+            foreach (Entry entry in _entries)
+            {
+                outputFile.WriteLine($"{entry._date}|{entry._prompt}|{entry._response}");
+            }
+        }
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        _entries.Clear();
+        string[] lines = File.ReadAllLines(filename);
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split('|');
+            Entry entry = new Entry
+            {
+                _date = parts[0],
+                _prompt = parts[1],
+                _response = parts[2]
+            };
+            AddEntry(entry);
+        }
+    }
+}
